86a826f0e388e52e7b3a4f20b5ed04f0db4e8dab
viylouu <honeefunnee@gmail.com>
2024-07-12 23:29:41 -0600

    uhhhhhhhh

--- src/games/plong/renderer.cs
+++ src/games/plong/renderer.cs
@@ -9,7 +9,7 @@
         c.DrawRect(paddleLpos, new Vector2(2, 16), Alignment.Center);
         c.DrawRect(paddleRpos, new Vector2(2, 16), Alignment.Center);
         c.DrawRect(ballpos, new Vector2(2, 2), Alignment.Center);
-                                                                                                                                                        
+
         c.FontSize(12);
         c.DrawText(lscore + "", new Vector2(16, 8), Alignment.Center);
         c.DrawText(rscore + "", new Vector2(144, 8), Alignment.Center);
